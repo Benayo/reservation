e9090eab8a0f42c1096bda571133e7933a3afac3
Benayo <ben_odeleye@yahoo.com>
2024-12-08 22:13:35 +0100

    latest changes

--- wwwroot/js/custom/header.js
+++ wwwroot/js/custom/header.js
@@ -1,4 +1,18 @@
-$(document).ready(function(){
+
+$(document).ready(function () {
+    var currentPath = window.location.pathname;
 
 
-})
+    $('.menu_nav .nav-item a').each(function () {
+        var linkPath = $(this).attr('href');
+
+       
+        if (currentPath.includes(linkPath)) {
+          
+            $('.menu_nav .nav-item').removeClass('active');
+
+       
+            $(this).parent().addClass('active');
+        }
+    });
+});
