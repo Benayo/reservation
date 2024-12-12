d8c7c0eea76be8e9a6339db5600be36fbb18af3a
Benayo <ben_odeleye@yahoo.com>
2024-12-12 12:20:24 +0100

    Latest Changes

--- wwwroot/js/custom/check-availabilty.js
+++ wwwroot/js/custom/check-availabilty.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-    // Initialize datepicker for both check-in and check-out fields
+    
     $('#date-in').datepicker({
-      dateFormat: 'mm/dd/yy', // Set the date format
-      minDate: 0, // Prevent selecting past dates
+      dateFormat: 'dd/mm/yy', 
+      minDate: 0, 
       onClose: function(selectedDate) {
-        // When a date is selected in the check-in field, set minDate for check-out field
+       
         $('#date-out').datepicker('option', 'minDate', selectedDate);
       }
     });
     
     $('#date-out').datepicker({
-      dateFormat: 'mm/dd/yy', // Set the date format
-      minDate: 1, // Ensure check-out date is always after check-in date
+      dateFormat: 'dd/mm/yy', 
+      minDate: 1, 
     });
   });
