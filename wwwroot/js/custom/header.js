db8af1f7cd707d2d0b8e354797c665e46f113316
Benayo <ben_odeleye@yahoo.com>
2024-12-13 11:08:26 +0100

    Latest Changes

--- wwwroot/js/custom/header.js
+++ wwwroot/js/custom/header.js
@@ -1,18 +1,43 @@
-
 $(document).ready(function () {
-    var currentPath = window.location.pathname;
+  var currentPath = window.location.pathname;
+
+  $(".menu_nav .nav-item a").each(function () {
+    var linkPath = $(this).attr("href");
+
+    if (currentPath.includes(linkPath)) {
+      $(".menu_nav .nav-item").removeClass("active");
+
+      $(this).parent().addClass("active");
+    }
+  });
+
+  $.getJSON('/appsettings.json', function(data){
+const email = data.contactInfo.email
+const phone = data.contactInfo.phone
 
 
-    $('.menu_nav .nav-item a').each(function () {
-        var linkPath = $(this).attr('href');
+$("#phone").text(phone)
+$("#phone-link").attr('href',`tel:${phone}` )
 
-       
-        if (currentPath.includes(linkPath)) {
-          
-            $('.menu_nav .nav-item').removeClass('active');
+$('#email').text(email)
+$('#email-link').attr('href',`tel:${email}`)
 
-       
-            $(this).parent().addClass('active');
-        }
-    });
+  }).fail(function () {
+    console.error("Failed to load settings.json");
+  });
+
+
+
+
+  $.getJSON('/appsettings.json', function(data){
+    const facebook = data.socialLinks.facebook
+    const twitter = data.socialLinks.twitter
+    const instagram = data.socialLinks.instagram
+
+    $('#facebook-link').attr('href', `${facebook}`)
+    $('#twitter-link').attr('href',`${twitter}`)
+    $('instagram-link').attr('href',`${instagram}`)
+  }).fail(function(){
+    console.error("Failed to load settings.json");
+  })
 });
